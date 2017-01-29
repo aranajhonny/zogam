0b1431db48626bcee255df8bfe389f9e3a0a8e10
aranajhonny <jhonnyjosearana@gmail.com>
2017-01-28 22:01:54 -0430

    add update vehiculo

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Vehiculo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class VehiculoController extends Controller
 {
@@ -15,7 +16,7 @@ class VehiculoController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+      $this->middleware('auth');
     }
 
     /**
@@ -26,9 +27,9 @@ class VehiculoController extends Controller
     public function index()
     {
 
-        $autos = DB::table('vehiculos')->orderBy('id', 'desc')->paginate(10);
+      $autos = DB::table('vehiculos')->orderBy('id', 'desc')->paginate(10);
 
-        return view('dashboard', ['autos' => $autos]);
+      return view('dashboard', ['autos' => $autos]);
     }
 
     public function create()
@@ -41,19 +42,56 @@ class VehiculoController extends Controller
         //
     }
 
-    public function edit(Vehiculo $vehiculo)
+    public function edit($id)
     {
-        //
+        // edit function here
+      $auto = Vehiculo::find($id);
+
+        // return to 404 page
+      if ( !$auto ) {
+        abort(404);
+      }
+        // display the article to single page
+      return view('vehiculos.edit')->with('auto',$auto);
     }
 
 
-    public function update(Request $request, Vehiculo $vehiculo)
+    public function update(Request $request, $id)
     {
-        //
+       Validator::make($request->all(), [
+        'placa'=> 'required',
+        'marca' => 'required',
+        'modelo' => 'required',
+        'anio' => 'required',
+        'serial_motor' => 'required',
+        'serial_carro' => 'required',
+        'color' => 'required',
+        'tipo' => 'required',
+        'propietario' => 'required',
+        'telf_prop' => 'required',
+        'email_prop' => 'required',
+        ])->validate();
+
+      $auto = Vehiculo::find($id);
+      $auto->placa = $request->placa;
+      $auto->marca = $request->marca;
+      $auto->modelo = $request->modelo;
+      $auto->anio = $request->anio;
+      $auto->serial_motor = $request->serial_motor;
+      $auto->serial_carro = $request->serial_carro;
+      $auto->color = $request->color;
+      $auto->tipo = $request->tipo;
+      $auto->propietario = $request->propietario;
+      $auto->telf_prop = $request->telf_prop;
+      $auto->email_prop = $request->email_prop;
+      // save all data
+      $auto->save();
+      //redirect page after save data
+      return redirect('vehiculo/'.$id.'/edit')->with('message','Ha sido actualizado exitosamente!');
     }
 
     public function destroy(Vehiculo $vehiculo)
     {
         //
     }
-}
+  }
---
+++ resources/views/vehiculos/edit.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    @if(Session::get('message'))
+        <div class="alert alert-success alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h5> {{ Session::get('message') }}</h5>
+        </div>
+    @endif
+    @if(Session::get('errors'))
+        <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h5>Complete los campos obligatorios.</h5>
+        </div>
+    @endif
+    <div class="col-md-6 col-md-offset-3">
+    <center><h3>Editar vehiculo</h3></center>
+    <br>
+     <form class="" action="/vehiculo/{{ $auto->id }}" method="post">
+
+      <div class="form-group @if ($errors->has('placa')) has-error @endif col-md-6">
+        <label for="">Placa</label>
+        <input type="text" class="form-control" name="placa" value="{{ $auto->placa }}" placeholder="Placa"><br>
+        @if ($errors->has('placa')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('marca')) has-error @endif col-md-6">
+        <label for="">Marca</label>
+        <input type="text" class="form-control" name="marca" value="{{ $auto->marca }}" placeholder="marca"><br>
+        @if ($errors->has('marca')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('modelo')) has-error @endif col-md-6">
+        <label for="">Modelo</label>
+        <input type="text" class="form-control" name="modelo" value="{{ $auto->modelo }}" placeholder="modelo"><br>
+        @if ($errors->has('modelo')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('anio')) has-error @endif col-md-6">
+        <label for="">Año</label>
+        <input type="text" class="form-control" name="anio" value="{{ $auto->anio }}" placeholder="año"><br>
+        @if ($errors->has('anio')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('serial_motor')) has-error @endif col-md-6">
+        <label for="">Serial del motor</label>
+        <input type="text" class="form-control" name="serial_motor" value="{{ $auto->serial_motor }}" placeholder="serial motor"><br>
+        @if ($errors->has('serial_motor')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('serial_carro')) has-error @endif col-md-6">
+        <label for="">Serial de la carroceria</label>
+        <input type="text" class="form-control" name="serial_carro" value="{{ $auto->serial_carro }}" placeholder="serial carro"><br>
+        @if ($errors->has('serial_carro')) <p class="help-block"> {Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('color')) has-error @endif col-md-6">
+        <label for="">Color</label>
+        <input type="text" class="form-control" name="color" value="{{ $auto->color }}" placeholder="color"><br>
+        @if ($errors->has('color')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('tipo')) has-error @endif col-md-6">
+        <label for="">Tipo de veh&iacuteculo</label>
+        <input type="text" class="form-control" name="tipo" value="{{ $auto->tipo }}" placeholder="tipo"><br>
+        @if ($errors->has('tipo')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('propietario')) has-error @endif col-md-6">
+        <label for="">Propietario</label>
+        <input type="text" class="form-control" name="propietario" value="{{ $auto->propietario }}" placeholder="propietario"><br>
+        @if ($errors->has('propietario')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('telf_prop')) has-error @endif col-md-6">
+      <label for="telefono">Tel&eacutefono</label>
+        <input type="text" class="form-control" name="telf_prop" value="{{ $auto->telf_prop }}" placeholder="telefono propietario"><br>
+        @if ($errors->has('telf_prop')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <div class="form-group @if ($errors->has('email_prop')) has-error @endif col-md-6">
+        <label for="">Correo electronico</label>
+        <input type="text" class="form-control" name="email_prop" value="{{ $auto->email_prop }}" placeholder="email propietario"><br>
+        @if ($errors->has('email_prop')) <p class="help-block"> Este campo es requerido.</p> @endif <br>
+      </div>
+
+      <input type="hidden" name="_method" value="put">
+
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+      <div class="form-group @if ($errors->has('name')) has-error @endif col-md-2 col-md-offset-5">
+        <input type="submit" name="name" value="Guardar">
+      </div>
+    </form>
+  </div>
+</div>
+</div>
+@endsection
