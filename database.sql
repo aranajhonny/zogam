f07871fef1f98774cad9b6d86f4f1c9a8493c4b4
aranajhonny <jhonnyjosearana@gmail.com>
2017-02-05 17:00:18 -0430

    add database

---
+++ database.sql
@@ -0,0 +1,231 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:8889
+-- Tiempo de generación: 05-02-2017 a las 22:29:38
+-- Versión del servidor: 5.6.34
+-- Versión de PHP: 7.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `zogan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `image_revs`
+--
+
+CREATE TABLE `image_revs` (
+  `image_id` int(10) UNSIGNED NOT NULL,
+  `revision_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('webmaster@gandocam.com.ve', '$2y$10$LwAxzXOzw3C0O5at4gHeXOsarPBqTcceOPaMbOJ7P4kny50Q4kpe6', '2017-02-05 20:13:37');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `revisions`
+--
+
+CREATE TABLE `revisions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `vehiculo_id` int(10) UNSIGNED NOT NULL,
+  `tipo` enum('desarmado','latoneria','pintura','pulitura','armado','revision') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'gandocam', 'webmaster@gandocam.com.ve', '$2y$10$LKX95bIKlAbtUgeqErsgvuhHRkrxTeOyD90QEsXWBfPZ/bYpWn4tW', 'EVMbu5t0z4SeCCYksFHLM8LNI6fi9doix49tVHn8shp4tmXFdX5nGMz0ZfG9', '2017-01-29 00:33:49', '2017-01-29 00:33:49');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculos`
+--
+
+CREATE TABLE `vehiculos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `placa` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `marca` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modelo` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `anio` int(11) NOT NULL,
+  `serial_motor` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `serial_carro` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `color` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `propietario` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telf_prop` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_prop` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `image_revs`
+--
+ALTER TABLE `image_revs`
+  ADD KEY `image_revs_image_id_foreign` (`image_id`),
+  ADD KEY `image_revs_rev_id_foreign` (`revision_id`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indices de la tabla `revisions`
+--
+ALTER TABLE `revisions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `revisions_id_veh_foreign` (`vehiculo_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indices de la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `vehiculos_placa_unique` (`placa`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `revisions`
+--
+ALTER TABLE `revisions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `image_revs`
+--
+ALTER TABLE `image_revs`
+  ADD CONSTRAINT `image_revs_image_id_foreign` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
+  ADD CONSTRAINT `image_revs_rev_id_foreign` FOREIGN KEY (`revision_id`) REFERENCES `revisions` (`id`);
+
+--
+-- Filtros para la tabla `revisions`
+--
+ALTER TABLE `revisions`
+  ADD CONSTRAINT `revisions_id_veh_foreign` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculos` (`id`);
